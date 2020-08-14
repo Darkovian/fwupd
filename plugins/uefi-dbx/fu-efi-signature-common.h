687477a053c15ae3ee9ef2db7074332383eb344a
Richard Hughes <richard@hughsie.com>
2020-08-14 10:16:36 +0100

    uefi-dbx: Check the binaries in the ESP are not in the dbx
    If found, fail the HSI runtime tests.

--- plugins/uefi-dbx/fu-efi-signature-common.h
+++ plugins/uefi-dbx/fu-efi-signature-common.h
@@ -10,3 +10,5 @@
 
 gboolean	 fu_efi_signature_list_array_inclusive	(GPtrArray	*outer,
 							 GPtrArray	*inner);
+gboolean	 fu_efi_signature_list_array_has_checksum (GPtrArray	*siglists,
+							 const gchar	*checksum);
